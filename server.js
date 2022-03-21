300fe60883b7920cb869cc391845b1e8547a6cd3
hendrychjan <jan.honza.hendrych@gmail.com>
2022-03-21 17:33:14 +0100

    Fix Heroku PORT timeout

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 //server.js
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const favicon = require("express-favicon");
+const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('Server up and running in ' + process.env.NODE_ENV + 'environment!');
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function (req, res) {
+  return res.send(
+    "Server up and running in " + process.env.NODE_ENV + "environment!"
+  );
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.PORT || 8080);
+
+const server = http.listen(port, () => {
+  console.log(`[SERVER] Server startup! listening on port ${port} in mode ${process.env.NODE_ENV}`);
+});
